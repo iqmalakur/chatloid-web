8e0af71a5570b216999ae8fd30dc99dcd8cf2df1
iqmalakur <iakbarkurnia@gmail.com>
2025-09-14 09:46:26 +0700

    feat: create color variables

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${interSans.className}`}>{children}</body>
+      <body className={`${interSans.className} bg-white`}>{children}</body>
     </html>
   );
 }
